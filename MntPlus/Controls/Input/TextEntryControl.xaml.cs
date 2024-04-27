5093914c62a16f588aca0880de860a889246fde2
Noureddine Zakhrouf <34780326+znono7@users.noreply.github.com>
2024-04-27 13:37:41 +0100

    Task Manager

---
+++ MntPlus/Controls/Input/TextEntryControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MntPlus.WPF
+{
+    /// <summary>
+    /// Interaction logic for TextEntryControl.xaml
+    /// </summary>
+    public partial class TextEntryControl : UserControl
+    {
+       
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TextEntryControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+      
+    }
+}
